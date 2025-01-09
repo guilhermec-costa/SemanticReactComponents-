370d2f11b44626b3843d6124df5cb372e4fb2562
Guilherme China <echina725@gmail.com>
2025-01-09 18:46:16 -0300

    minor fixed

--- packages/semanticComponents/vite.config.ts
+++ packages/semanticComponents/vite.config.ts
@@ -1,32 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import dts from 'vite-plugin-dts';
-
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), dts()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')  // Alias @ para o src do package
-    }
-  },
   build: {
-    outDir: 'dist',  // Local onde a build será gerada para o package
     lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),  // Ponto de entrada da build
-      name: 'SemanticComponents',
-      fileName: (format) => `semantic-components.${format}.js`,
+      entry: "./src/index.ts",
+      name: "SemanticComponents",
+      fileName: (format) => {
+        if (format === "cjs") return "index.js"; // Renomeia o arquivo cjs para index.js
+        return `semantic-components.${format}.js`;
+      },
+      formats: ["es", "umd", "cjs"],
     },
+    outDir: "./dist",
     rollupOptions: {
-      // Se precisar de dependências externas, pode especificá-las aqui
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom"], // Exclua dependências externas
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+          react: "React",
+          "react-dom": "ReactDOM",
         },
       },
     },
   },
+  plugins: [
+    dts({
+      insertTypesEntry: true, // Garante que tipos sejam gerados
+    }),
+  ],
 });
