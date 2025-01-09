ba5a58a35942aa2f385e81717bf645b4a7a71deb
Guilherme China <echina725@gmail.com>
2025-01-09 19:04:34 -0300

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,4 @@
 import { defineConfig } from "vite";
 
-import dts from "vite-plugin-dts";
-
-export default defineConfig({
-  build: {
-    lib: {
-      entry: "./packages/semanticComponents/src/index.ts",
-      name: "SemanticReactUIComponents",
-      fileName: (format) => `semantic-components.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-  plugins: [
-    dts({
-      outDir: "./packages/semanticComponents/dist",
-      insertTypesEntry: true,
-    }),
-  ],
-});
+// https://vitejs.dev/config/
+export default defineConfig({});
